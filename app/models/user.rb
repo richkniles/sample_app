97f22d960980f7fa5befc52fc707716e90eb6c6b
Rich Niles <richkniles@gmail.com>
2012-06-16 20:30:22 -0700

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,20 @@ class User < ActiveRecord::Base
   has_secure_password
   
   before_save { self.email.downcase! }
+  before_save :create_remember_token
   
   validates :name,  presence: true, length: { maximum: 50 }
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 } # presence: true not needed because password_digest covers this
+                                              # automatically in has_secure_password.  Changed the message created
+                                              # for password_digest to password in the localization file in config/locales
   validates :password_confirmation, presence: true
+  
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,6 +23,82 @@ describe "User pages" do
         expect { click_button submit }.not_to change(User, :count)
       end
       
+      describe "after submission" do
+        
+        describe "with no user name" do
+          before do
+            fill_in "Name",         with: " "
+            fill_in "Email",        with: "user@example.com"
+            fill_in "Password",     with: "foobar"
+            fill_in "Password confirmation", with: "foobar"
+            click_button submit
+          end 
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content("Name can't be blank")}
+        end
+        
+        describe "with no email" do
+          before do
+            fill_in "Name",         with: "Example Jo"
+            fill_in "Email",        with: " "
+            fill_in "Password",     with: "foobar"
+            fill_in "Password confirmation", with: "foobar"
+            click_button submit
+          end 
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content("Email can't be blank")}
+        end
+        
+        describe "with invalid email" do
+          before do
+            fill_in "Name",         with: "Example Jo"
+            fill_in "Email",        with: "mary$mary.com"
+            fill_in "Password",     with: "foobar"
+            fill_in "Password confirmation", with: "foobar"
+            click_button submit
+          end 
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content("Email is invalid")}
+        end
+                
+        describe "with no password" do
+          before do
+            fill_in "Name",         with: "Example Jo"
+            fill_in "Email",        with: "mary$mary.com"
+            fill_in "Password",     with: " "
+            fill_in "Password confirmation", with: "foobar"
+            click_button submit
+          end 
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content("Password can't be blank")}
+        end
+
+        describe "with short password" do
+          before do
+            fill_in "Name",         with: "Example Jo"
+            fill_in "Email",        with: "mary$mary.com"
+            fill_in "Password",     with: "fooba"
+            fill_in "Password confirmation", with: "fooba"
+            click_button submit
+          end 
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content("Password is too short")}
+        end
+
+        describe "with password confirmation not matching password" do
+          before do
+            fill_in "Name",         with: "Example Jo"
+            fill_in "Email",        with: "mary$mary.com"
+            fill_in "Password",     with: "foobar"
+            fill_in "Password confirmation", with: "foobas"
+            click_button submit
+          end 
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content("Password doesn't match confirmation ")}
+        end
+        
+      end
+      
     end
     
     describe "with valid information" do
@@ -34,6 +110,29 @@ describe "User pages" do
       end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count)
+      end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let (:user) { User.find_by_email('user@example.com') }
+        
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+        
+        describe "try to create another user with same email" do
+          before do 
+            visit signup_path
+            fill_in "Name",         with: "Example User"
+            fill_in "Email",        with: "user@example.com"
+            fill_in "Password",     with: "foobar"
+            fill_in "Password confirmation", with: "foobar"
+            click_button submit
+          end
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content("Email has already been taken")}
+          
+        end
       end
     end
     
